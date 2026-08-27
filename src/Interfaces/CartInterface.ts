f77495b5967eb7ae81e9e62fa970643b6cc725b0
ArsenyoP <arsenyo198510@gmail.com>
2026-08-27 18:55:19 +0300

    Created interface for expanded cart with products array

--- src/Interfaces/CartInterface.ts
+++ src/Interfaces/CartInterface.ts
@@ -1,3 +1,5 @@
+import type { ProductInterface } from "./ProductInterface";
+
 export interface CartItem {
     id: number;
     productId: string;
@@ -7,6 +9,10 @@ export interface CartItem {
     updatedAt: string;
   }
 
+export interface CartExpanded{
+  cartItem: CartItem;
+  products: ProductInterface[]
+}
 
 export interface CartInterface{
   Items: CartItem[]
