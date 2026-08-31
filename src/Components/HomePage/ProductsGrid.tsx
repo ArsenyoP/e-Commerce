7e66fb8f4d0cfcc8aec3b86b247c0622a90f3dfc
ArsenyoP <arsenyo198510@gmail.com>
2026-08-31 18:17:35 +0300

    Added Product component

--- src/Components/HomePage/ProductsGrid.tsx
+++ src/Components/HomePage/ProductsGrid.tsx
@@ -1,5 +1,5 @@
 import type { ProductInterface } from "../../Interfaces/ProductInterface";
-import axios from "axios";
+import { Product } from "./Product";
 
 interface ProductsGridProps{
     products: ProductInterface[]
@@ -9,66 +9,9 @@ interface ProductsGridProps{
 export const ProductsGrid = ({products, fetchCart}: ProductsGridProps) =>{
     return <>
         <div className="products-grid">
-          {products.map((product) => {
-            return (
-              <div key={product.id} className="product-container">
-                <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
-                </div>
-
-                <div className="product-name limit-text-to-2-lines">
-                  {product.name}
-                </div>
-
-                <div className="product-rating-container">
-                  <img
-                    className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars*10}.png`}
-                  />
-                  <div className="product-rating-count link-primary">87</div>
-                </div>
-
-                <div className="product-price">{(product.priceCents/100).toFixed(2)}$</div>
-
-                <div className="product-quantity-container">
-                  <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                  </select>
-                </div>
-
-                <div className="product-spacer"></div>
-
-                <div className="added-to-cart">
-                  <img src="images/icons/checkmark.png" />
-                  Added
-                </div>
-
-                <button className="add-to-cart-button button-primary"
-                  onClick={async () => {
-                    try {
-                      await axios.post('http://localhost:3000/api/cart-items', { productId: product.id, quantity: 1 });
-                      await fetchCart();
-                    } catch (error) {
-                      console.error('Error adding to cart:', error);
-                    }
-                  }}>
-                  Add to Cart
-                </button>
-              </div>
-            );
-          })}
+          {products.map((product) => (
+            <Product key={product.id} product={product} fetchCart={fetchCart} />
+          ))}
         </div>
     </>
 }
