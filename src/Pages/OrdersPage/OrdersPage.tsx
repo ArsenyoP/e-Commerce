621588f0f0549b5c6b7ce955415ccb15f3b760b9
ArsenyoP <arsenyo198510@gmail.com>
2026-09-01 22:38:03 +0300

    Generating orders from backend

--- src/Pages/OrdersPage/OrdersPage.tsx
+++ src/Pages/OrdersPage/OrdersPage.tsx
@@ -2,8 +2,36 @@ import "../../Components/Header/Header.css";
 import Header from "../../Components/Header/Header";
 import type { CartQuantityProps } from "../../Interfaces/CartQuantityProps";
 import "./OrdersPage.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import type { OrderInterface } from "../../Interfaces/OrderInterfaces";
+import { formatMoney } from "../../utils/money";
+import { formatDate } from "../../utils/dateTime";
 
 export const OrdersPage = ({cartQuantity}: CartQuantityProps) => {
+
+  const [orders, setOrders] = useState<OrderInterface[]>([]);
+
+  const fetchOrders = async () =>{
+    let response = await axios.get<OrderInterface[]>("http://localhost:3000/api/orders?expand=products")
+    setOrders(response.data)
+  }
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await axios.get<OrderInterface[]>(
+          "http://localhost:3000/api/orders?expand=products"
+        );
+        setOrders(response.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+  
+    fetchOrders();
+  }, []);
+
   return (
     <>
       <title>Orders</title>
@@ -14,81 +42,9 @@ export const OrdersPage = ({cartQuantity}: CartQuantityProps) => {
         <div className="page-title">Your Orders</div>
 
         <div className="orders-grid">
-          <div className="order-container">
-            <div className="order-header">
-              <div className="order-header-left-section">
-                <div className="order-date">
-                  <div className="order-header-label">Order Placed:</div>
-                  <div>August 12</div>
-                </div>
-                <div className="order-total">
-                  <div className="order-header-label">Total:</div>
-                  <div>$35.06</div>
-                </div>
-              </div>
 
-              <div className="order-header-right-section">
-                <div className="order-header-label">Order ID:</div>
-                <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
-              </div>
-            </div>
-
-            <div className="order-details-grid">
-              <div className="product-image-container">
-                <img src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-              </div>
-
-              <div className="product-details">
-                <div className="product-name">
-                  Black and Gray Athletic Cotton Socks - 6 Pairs
-                </div>
-                <div className="product-delivery-date">
-                  Arriving on: August 15
-                </div>
-                <div className="product-quantity">Quantity: 1</div>
-                <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                  <span className="buy-again-message">Add to Cart</span>
-                </button>
-              </div>
-
-              <div className="product-actions">
-                <a href="/tracking">
-                  <button className="track-package-button button-secondary">
-                    Track package
-                  </button>
-                </a>
-              </div>
-
-              <div className="product-image-container">
-                <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-              </div>
-
-              <div className="product-details">
-                <div className="product-name">
-                  Adults Plain Cotton T-Shirt - 2 Pack
-                </div>
-                <div className="product-delivery-date">
-                  Arriving on: August 19
-                </div>
-                <div className="product-quantity">Quantity: 2</div>
-                <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                  <span className="buy-again-message">Add to Cart</span>
-                </button>
-              </div>
-
-              <div className="product-actions">
-                <a href="/tracking">
-                  <button className="track-package-button button-secondary">
-                    Track package
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="order-container">
+          {orders.map((order) => {
+          return  <div className="order-container" key={order.id}>
             <div className="order-header">
               <div className="order-header-left-section">
                 <div className="order-date">
@@ -97,29 +53,31 @@ export const OrdersPage = ({cartQuantity}: CartQuantityProps) => {
                 </div>
                 <div className="order-total">
                   <div className="order-header-label">Total:</div>
-                  <div>$41.90</div>
+                  <div>{formatMoney(order.totalCostCents)}</div>
                 </div>
               </div>
 
               <div className="order-header-right-section">
                 <div className="order-header-label">Order ID:</div>
-                <div>b6b6c212-d30e-4d4a-805d-90b52ce6b37d</div>
+                <div>{order.id}</div>
               </div>
             </div>
 
-            <div className="order-details-grid">
+
+            {order.products.map((product) => {
+              return <div className="order-details-grid">
               <div className="product-image-container">
-                <img src="images/products/intermediate-composite-basketball.jpg" />
+                <img src={product.product.image} />
               </div>
 
               <div className="product-details">
                 <div className="product-name">
-                  Intermediate Size Basketball
+                  {product.product.name}
                 </div>
                 <div className="product-delivery-date">
-                  Arriving on: June 17
+                  Arriving on: {formatDate(product.estimatedDeliveryTimeMs)}
                 </div>
-                <div className="product-quantity">Quantity: 2</div>
+                <div className="product-quantity">Quantity: {product.quantity}</div>
                 <button className="buy-again-button button-primary">
                   <img className="buy-again-icon" src="images/icons/buy-again.png" />
                   <span className="buy-again-message">Add to Cart</span>
@@ -134,7 +92,14 @@ export const OrdersPage = ({cartQuantity}: CartQuantityProps) => {
                 </a>
               </div>
             </div>
+            })}
+            
+
           </div>
+          })}
+          
+
+
         </div>
       </div>
     </>
